0e6e1e847b81b8b531c667e28c1b4ed9fa4e585a
Migara Bhanu <76873393+migarabhanu@users.noreply.github.com>
2021-11-07 12:09:50 +0530

    llib.js created

---
+++ lib.js
@@ -0,0 +1,224 @@
+var jsFile = document.createElement("script");
+jsFile.src = "homePage.js";  // it can be path also  "{themes('/scripts/homePage.js')}"
+document.body.appendChild(jsFile);  //append it as src to bodyvar jsFile = document.createElement("script");
+jsFile.src = "homePage.js";  // it can be path also  "{themes('/scripts/homePage.js')}"
+document.body.appendChild(jsFile);  //append it as src to body
+
+<script src="/path/to/script.js"></script>
+<script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script><script src="file.js"></script>
+<script>
+  alert(1);
+</script>var jsFile = document.createElement("script");
+jsFile.src = "homePage.js";  // it can be path also  "{themes('/scripts/homePage.js')}"
+document.body.appendChild(jsFile);  //append it as src to bodyvar jsFile = document.createElement("script");
+jsFile.src = "homePage.js";  // it can be path also  "{themes('/scripts/homePage.js')}"
+document.body.appendChild(jsFile);  //append it as src to bodyvar jsFile = document.createElement("script");
+jsFile.src = "homePage.js";  // it can be path also  "{themes('/scripts/homePage.js')}"
+document.body.appendChild(jsFile);  //append it as src to body
